92a70bf317897620939def9fd770c00d0e3e210b
maddysewed <maddysewed@gmail.com>
2021-09-26 18:53:11 +0300

    Deleting previous task

--- TatyanaAntonova/5.1.py
+++
@@ -1,8 +0,0 @@
-def main():
-    with open("C:\\Users\H P\\Desktop\\code\\PYTHON\\EPAM_training\\Session_5\\unsorted_names.txt", "r") as file_from:
-        with open("C:\\Users\H P\\Desktop\\code\\PYTHON\\EPAM_training\\Session_5\\sorted_names.txt", "a") as file_to:
-            for line in sorted(file_from.readlines()):
-                file_to.write(f"{line}")
-
-if __name__ == "__main__":
-    main()
